c50f02a625f1f80c33588f68dd3e1db7de844485
Belinskaya Tamara <163990978+tlinskk@users.noreply.github.com>
2025-03-11 14:58:40 +0200

    Pr1(№5.б)

---
+++ lab_work_02_02/lab_work_02_02/lab_work_02_02.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <windows.h>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const double pi = 3.14159;
+    double x, y, z;
+    double b1, b2, b;
+    cout << " Введiть значення трьох дiйсних змiнних:\n ";
+    cin >> x >> y >> z;
+
+    b1 = log(1 + x * x + y * y + z * z) + exp(-2.1 + x + y);
+    b2 = (3.0 / 4.0) * (1 + (x * x / 2 )+( y * y / 4) + (z * z / 8)) * (0.1 + pow(cos(x - y), 2));
+    b = pow(fabs(b1 / b2),3);
+    cout << fixed << setprecision(4);
+    cout << "\nРезультати обчислень:";
+    cout << "\n---------------------------------";
+    cout << "\nВхідні значення:";
+    cout << "\n---------------------------------";
+    cout << "\n x = " << x;
+    cout << "\n y = " << y;
+    cout << "\n z = " << z;
+    cout << "\n---------------------------------";
+    cout << "\nПроміжні значення:";
+    cout << "\n---------------------------------";
+    cout << "\n b1 = " << b1;
+    cout << "\n b2 = " << b2;
+    cout << "\n---------------------------------";
+    cout << "\n Остаточне значення:";
+    cout << "\n b = " << b;
+    cout << "\n---------------------------------\n";
+    _getch();
+    return 0;
+}
